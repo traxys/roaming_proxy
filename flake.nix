1d0907525bff7b65524dbd826e17ec0ade723c8e
traxys <quentin+dev@familleboyer.net>
2022-09-07 23:22:28 +0200

    Allow to build the pac_proxy with nix

--- flake.nix
+++ flake.nix
@@ -1,16 +1,34 @@
 {
   description = "A basic flake with a shell";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.pacparser.url = "github:traxys/pacparser-rs";
+  inputs.naersk.url = "github:nix-community/naersk";
 
   outputs = {
     self,
-    nixpkgs,
     flake-utils,
     pacparser,
+    naersk,
   }:
-    flake-utils.lib.eachDefaultSystem (system: {
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import pacparser.inputs.nixpkgs {
+        inherit system;
+
+        overlays = [(import pacparser.inputs.rust-overlay)];
+      };
+      rust = pkgs.rust-bin.stable.latest.default;
+
+      naersk' = pkgs.callPackage naersk {
+        cargo = rust;
+        rustc = rust;
+      };
+    in {
       devShell = pacparser.devShell."${system}";
+
+      defaultPackage = naersk'.buildPackage {
+        src = ./.;
+        gitSubmodules = true;
+        buildInputs = [pkgs.rustPlatform.bindgenHook];
+      };
     });
 }
